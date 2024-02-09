8648e93cc3d457b145183695d948156f84a42111
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2024-02-09 12:54:36 +0100

    Updated startup scripts and port. Added venv folder to gitignore

--- streamlit_pianoroll/__init__.py
+++ streamlit_pianoroll/__init__.py
@@ -1,6 +1,6 @@
 import os
-import pandas as pd
 
+import pandas as pd
 from fortepyan import MidiPiece
 import streamlit.components.v1 as components
 
@@ -30,7 +30,7 @@ if not _RELEASE:
         # Pass `url` here to tell Streamlit that the component will be served
         # by the local dev server that you run via `npm run start`.
         # (This is useful while your component is in development.)
-        url="http://localhost:3001",
+        url="http://localhost:3210",
     )
 else:
     # When we're distributing a production version of the component, we'll
